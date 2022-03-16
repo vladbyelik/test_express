34304171339cd0fe96c00108c80df83f9b779afb
vladbyelik <vladbyelik@gmail.com>
2022-03-16 23:30:17 +0200

    db connected!

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors');
 
 // create connection
 
+const app = express();
+app.use(cors());
 
 const db = mysql.createConnection({
     host: '127.0.0.1',
     user: 'vlad',
-    password: '******',
-    database: 'commdiva',
+    password: '1234',
+    database: 'test_db',
     port: 3306,
 });
 
@@ -21,19 +24,42 @@ db.connect((err) => {
   console.log('Database connected!');
 });
 
-const app = express();
-
 // create db
 
 app.get('/create', (req, res) => {
   // let sql = 'CREATE DATABASE nodemysql';
   
-  let sql = 'SELECT * FROM user';
+  let sql = 'SELECT * FROM test_table';
 
   db.query(sql, (err, result) => {
     if(err) throw err;
     console.log(result);
-    res.send('db created!');
+    res.send(result);
+  });
+});
+
+app.get('/add', (req, res) => {
+  // let sql = 'CREATE DATABASE nodemysql';
+
+  const data = { id: 3, name: 'Vlad', surname: 'Bielik' }
+  
+  let sql = 'INSERT INTO test_table SET ?';
+
+  db.query(sql, data, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.send('POST created!');
+  })
+});
+
+app.get('/add/:id', (req, res) => { 
+  let sql = `SELECT * FROM test_table WHERE id = ${req.params.id}`;
+
+  db.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.send(result);
+    // res.json({ res: 'result' });
   })
 });
 
